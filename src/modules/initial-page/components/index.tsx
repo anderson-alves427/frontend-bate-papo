a174d49863bb02e15aa484c728137f0902956754
anderson-alves427 <oliveiraalves427@gmail.com>
2022-12-11 01:04:33 -0300

    Salvando link digitado em estado

--- src/modules/initial-page/components/index.tsx
+++ src/modules/initial-page/components/index.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { Container } from "../../../shared/components/container";
 
 import "./style.css";
 
 export const InitialPage = () => {
+    const [textLink, setTextLink] = useState("");
+
+    const handleChangeTextLink = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTextLink(event.target.value);
+    }
+
     return (
         <Container>
             <main className="main-initial-page">
@@ -11,8 +18,13 @@ export const InitialPage = () => {
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum id commodi aspernatur doloribus fugiat numquam molestiae impedit ipsam incidunt officia. Dolorem sunt voluptatibus tenetur nemo quis sequi rem cumque enim?</p>
                     <div className="buttons-links">
                         <button>Nova Reunião</button>
-                        <input type="text" placeholder="Digite um código de link"/>
-                        <button>Participar</button>
+                        <input 
+                            type="text" 
+                            placeholder="Digite um código de link" 
+                            onChange={handleChangeTextLink}
+                            value={textLink} 
+                        />
+                        <button disabled={!textLink.length} onClick={() => console.log(textLink)}>Participar</button>
                     </div>
                 </aside>
                 <aside className="aside-right">
